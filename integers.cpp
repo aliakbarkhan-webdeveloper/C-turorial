ed738fb96f8481baeba42375c75ed3cb4b8fb8df
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-12 20:45:42 +0500

    integers

---
+++ integers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main(){
+
+cout<<sizeof(uint64_t)*8<<"\n";
+cout<<sizeof(int)*8<<"\n";
+cout<<sizeof(short)*8<<"\n";
+cout<<sizeof(long)*8<<"\n";
+cout<<sizeof(long long)*8<<"\n";
+
+
+    return 0;
+}
