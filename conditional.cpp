8352f43740c9ff36ed5bd671ab63008bfec0023d
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-12 23:47:15 +0500

    conditionals

--- conditional.cpp
+++ conditional.cpp
@@ -18,8 +18,31 @@ int main()
     //    }
 
     // Ternary operator
-    int age = 17;
-    age >= 18 ? cout << "you are eligible for marriage" : cout << "sorry you have to wait for " << 18 - age << " years for marriage";
+    // int age = 17;
+    // age >= 18 ? cout << "you are eligible for marriage" : cout << "sorry you have to wait for " << 18 - age << " years for marriage";
+
+    // Switch case
+
+    int rating = 2;
+    switch (rating)
+    {
+    case 1:
+        cout << "so bad to know you don't like our product" << "";
+        break;
+    case 2:
+        cout << "so bad to know you don't like our product" << "";
+        break;
+    case 3:
+        cout << "your feedback will be good to improve our product" << "";
+        break;
+    case 4:
+        cout << "good to know you like our product we will be improve our product according to your feedback" << "";
+        break;
+
+    default:
+        cout << "Good hogya, done hogya te chass agae" << "";
+        break;
+    }
 
     return 0;
 }
