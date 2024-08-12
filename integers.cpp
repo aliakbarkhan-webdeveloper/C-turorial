dda5ae3ef1986056b665d0b283d9a42b699d99d4
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-12 23:18:35 +0500

    integer

--- integers.cpp
+++ integers.cpp
@@ -2,14 +2,14 @@
 #include <cstdint>
 using namespace std;
 
-int main(){
+int main()
+{
 
-cout<<sizeof(uint64_t)*8<<"\n";
-cout<<sizeof(int)*8<<"\n";
-cout<<sizeof(short)*8<<"\n";
-cout<<sizeof(long)*8<<"\n";
-cout<<sizeof(long long)*8<<"\n";
-
+    cout << sizeof(uint64_t) * 8 << "\n";
+    cout << sizeof(int) * 8 << "\n";
+    cout << sizeof(short) * 8 << "\n";
+    cout << sizeof(long) * 8 << "\n";
+    cout << sizeof(long long) * 8 << "\n";
 
     return 0;
 }
