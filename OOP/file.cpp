3472fc51bf2a504b9269228dbf0830692fd5a19b
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-19 14:08:12 +0500

    file handeling

--- OOP/file.cpp
+++ OOP/file.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main()
 {
-    //create file
-   FILE *myF=fopen("originalFile.txt","w");
-   fclose(myF);
+    // create file
+    FILE *myF = fopen("originalFile.txt", "w");
+    fclose(myF);
 
-   //rename  file
-   rename("originalFile.txt","newFile.cpp");
-   //removing file
-   remove("newFile.txt");
+    // rename  file
+    rename("originalFile.txt", "newFile.cpp");
+    // removing file
+    remove("newFile.txt");
     return 0;
 }
